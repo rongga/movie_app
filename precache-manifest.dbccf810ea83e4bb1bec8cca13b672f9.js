c7a406ba7b5534042ac4c0638630d37f81fa8c08
rongga <jyj1103_@naver.com>
2020-04-17 15:52:23 +0900

    Updates

---
+++ precache-manifest.dbccf810ea83e4bb1bec8cca13b672f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b4ef8299f094228117a7416b084fa8a",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "5471017ee0bbd6eb7f8e",
+    "url": "/movie_app/static/css/main.837b6f30.chunk.css"
+  },
+  {
+    "revision": "697e48abbd7f0ba75f99",
+    "url": "/movie_app/static/js/2.75f33243.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.75f33243.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5471017ee0bbd6eb7f8e",
+    "url": "/movie_app/static/js/main.d1aa344c.chunk.js"
+  },
+  {
+    "revision": "77df4e0b724824eca1cd",
+    "url": "/movie_app/static/js/runtime-main.0b3e8895.js"
+  }
+]);
